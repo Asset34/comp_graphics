2f2af8bf3af305abc6944a5476603c735fdac313
Artemiy Kashintsev <artemiy.kashintsev@gmail.com>
2024-03-26 17:19:30 +0300

    Implement Camera

---
+++ include/camera.h
@@ -0,0 +1,41 @@
+#ifndef CAMERA_H_
+#define CAMERA_H_
+
+#include "sceneobj.h"
+
+class Camera : public SceneObj {
+public:
+    Camera(float hfov = 45.0, float aspectRatio = 1.0, float near = 0.01, float far = 100.0);
+    ~Camera();
+
+    const mat4 &getViewMatrix() const;
+    const mat4 &getProjectionMatrix() const;
+
+    void setAspectRatio(float aspectRatio);
+    void setHFov(float hfov);
+    void setVolume(float hfov, float aspectRatio, float near, float far);
+    void setNoProjection();
+    void setOrthoProjection();
+    void setPerspectiveProjection();
+
+private:
+    void computeTop(float hfov);
+    void computeRight(float aspectRatio);
+    void computeProjection();
+
+    enum ProjType {
+        No,
+        Ortho,
+        Perspective
+    };
+
+    mat4 m_viewMatrix;
+    mat4 m_projMatrix;
+    ProjType m_projType;
+
+    float m_right, m_top;
+    float m_near, m_far;
+
+};
+
+#endif // CAMERA_H_
---
+++ src/camera.cpp
@@ -0,0 +1,106 @@
+#include "camera.h"
+
+#include <cmath>
+
+Camera::Camera(float hfov, float aspectRatio, float near, float far)
+    :m_viewMatrix(1.0f),
+     m_projMatrix(1.0f),
+     m_projType(ProjType::No)
+{
+    this->setVolume(hfov, aspectRatio, near, far);
+}
+
+Camera::~Camera()
+{
+}
+
+const mat4 &Camera::getViewMatrix() const
+{
+    return m_viewMatrix;
+}
+
+const mat4 &Camera::getProjectionMatrix() const
+{
+    return m_projMatrix;
+}
+
+void Camera::setAspectRatio(float aspectRatio)
+{
+    this->computeRight(aspectRatio);
+    this->computeProjection();
+}
+
+void Camera::setHFov(float hfov)
+{
+    this->computeTop(hfov);
+    this->computeProjection();
+}
+
+void Camera::setVolume(float hfov, float aspectRatio, float near, float far)
+{
+    m_near = near;
+    m_far = far;
+
+    this->computeTop(hfov);
+    this->computeRight(aspectRatio);
+    this->computeProjection();
+}
+
+void Camera::setNoProjection()
+{
+    m_projType = ProjType::No;
+    this->computeProjection();
+}
+
+void Camera::setOrthoProjection()
+{
+    m_projType = ProjType::Ortho;
+    this->computeProjection();
+}
+
+void Camera::setPerspectiveProjection()
+{
+    m_projType = ProjType::Perspective;
+    this->computeProjection();
+}
+
+void Camera::computeTop(float hfov)
+{
+    float hfovRadians = glm::radians(hfov);
+    m_top = glm::tan(hfovRadians / 2.0) * m_near;
+}
+
+void Camera::computeRight(float aspectRatio)
+{
+    m_right = m_top * aspectRatio;
+}
+
+void Camera::computeProjection()
+{
+    // Intermediate values
+    float d = m_far - m_near;
+
+    m_projMatrix = mat4(0.0f);
+    switch (m_projType)
+    {
+        case No:
+            m_projMatrix = mat4(1.0f);
+        break;
+
+        case Ortho:
+            m_projMatrix[0][0] = 1.0 / m_right;
+            m_projMatrix[1][1] = 1.0 / m_top;
+            m_projMatrix[2][2] = -2.0 / d;
+            m_projMatrix[3][3] = 1;
+            m_projMatrix[3][2] = - (m_far + m_near) / d;
+        break;
+
+        case Perspective:
+            m_projMatrix[0][0] = m_near / m_right;
+            m_projMatrix[1][1] = m_near / m_top;
+            m_projMatrix[2][2] = - (m_near + m_far) / d;
+            m_projMatrix[2][3] = -1;
+            m_projMatrix[3][2] = - 2 * m_far * m_near / d;
+        break;
+    }
+}
