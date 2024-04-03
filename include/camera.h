2cd9c27b565101a604e3a5fdb0012628311f64b7
Artemiy Kashintsev <artemiy.kashintsev@gmail.com>
2024-04-03 09:31:48 +0300

    Camera: redone (without zoom)

--- include/camera.h
+++ include/camera.h
@@ -12,16 +12,31 @@ public:
     const mat4 &getProjectionMatrix() const;
 
     void setAspectRatio(float aspectRatio);
-    void setHFov(float hfov);
+    void setFov(float fov);
     void setVolume(float hfov, float aspectRatio, float near, float far);
+    
     void setNoProjection();
     void setOrthoProjection();
     void setPerspectiveProjection();
 
-    void lookAt(const vec3 &point = vec3(0.0f, 0.0f, 0.0f));
+    void setYawLimits(float min, float max);
+    void setPitchLimits(float min, float max);
+    // void setZoomLimits(float min, float max);
+
+    void rotateYaw(float angle);
+    void rotatePitch(float angle);
+
+    void ZoomIn();
+    void ZoomOut();
+
+    virtual SceneObjData compileData() const override;
+
+protected:
+    virtual void transformationCallback() override;
 
 private:
-    void computeTop(float hfov);
+    void lookAt(const vec3 &point = {0, 0, 0});
+    void computeTop(float fov);
     void computeRight(float aspectRatio);
     void computeProjection();
 
@@ -35,8 +50,23 @@ private:
     mat4 m_projMatrix;
     ProjType m_projType;
 
-    float m_right, m_top;
-    float m_near, m_far;
+    static constexpr const float ZOOM_IN_FACTOR = 0.9;
+    static constexpr const float ZOOM_OUT_FACTOR = 0.9;
+
+    // Camera values
+    float m_yaw, m_pitch;
+
+    // Volume
+    float m_right, m_top, m_near, m_far;
+
+    // Limits
+    float m_yawMin, m_yawMax;
+    float m_pitchMin, m_pitchMax;
+    // float m_zoomMin, m_zoomMax;
+
+    // Flags
+    bool m_yawLimit, m_pitchLimit, m_zoomLimit;
+    bool m_callbackOccured;
 
 };
 
--- src/camera.cpp
+++ src/camera.cpp
@@ -3,7 +3,13 @@
 Camera::Camera(float hfov, float aspectRatio, float near, float far)
     :m_viewMatrix(1.0f),
      m_projMatrix(1.0f),
-     m_projType(ProjType::No)
+     m_projType(ProjType::No),
+     m_yaw(0),
+     m_pitch(0),
+     m_yawLimit(false),
+     m_pitchLimit(false),
+     m_zoomLimit(false),
+     m_callbackOccured(false)
 {
     this->setVolume(hfov, aspectRatio, near, far);
 }
@@ -15,10 +21,14 @@ Camera::~Camera()
 const mat4 &Camera::getViewMatrix()
 {
     mat4 translateMatrix(1.0f);
-    vec3 pos = this->getPosition();
+    vec3 pos = this->getOrigin();
     translateMatrix[3] = vec4(-pos, 1.0f);
 
-    mat4 rotateMatrix = glm::transpose(this->getUnits());
+    mat4 rotateMatrix(1.0f);
+    rotateMatrix[0] = vec4(this->getUnitx(), 0);
+    rotateMatrix[1] = vec4(this->getUnity(), 0);
+    rotateMatrix[2] = vec4(this->getUnitz(), 0);
+    rotateMatrix = glm::transpose(rotateMatrix);
 
     m_viewMatrix = rotateMatrix * translateMatrix;
     return m_viewMatrix;
@@ -35,9 +45,9 @@ void Camera::setAspectRatio(float aspectRatio)
     this->computeProjection();
 }
 
-void Camera::setHFov(float hfov)
+void Camera::setFov(float fov)
 {
-    this->computeTop(hfov);
+    this->computeTop(fov);
     this->computeProjection();
 }
 
@@ -69,16 +79,103 @@ void Camera::setPerspectiveProjection()
     this->computeProjection();
 }
 
-void Camera::lookAt(const vec3 &point)
+void Camera::setYawLimits(float min, float max)
 {
-    vec3 vector = this->getPosition() - point;
-    this->coincidez(vector);
+    m_yawMin = min;
+    m_yawMax = max;
 }
 
-void Camera::computeTop(float hfov)
+void Camera::setPitchLimits(float min, float max)
 {
-    float hfovRadians = glm::radians(hfov);
-    m_top = glm::tan(hfovRadians / 2.0) * m_near;
+    m_pitchMin = min;
+    m_pitchMax = max;
+}
+
+// void Camera::setZoomLimits(float min, float max)
+// {
+//     m_zoomMin = min;
+//     m_zoomMax = max;
+// }
+
+void Camera::rotateYaw(float angle)
+{
+    if (m_yaw > m_yawMin && m_yaw < m_yawMax) {
+        // Compute new angle
+        float newYaw = m_yaw + angle;
+        if (newYaw < m_yawMin) {
+            newYaw = m_yawMin;
+        }
+        if (newYaw > m_yawMax) {
+            newYaw = m_yawMax;
+        }
+
+        // Rotate
+        float rotationAngle = newYaw - m_yaw;
+        this->rotatey(rotationAngle);
+
+        // Update yaw
+        m_yaw = newYaw;
+    }
+}
+
+void Camera::rotatePitch(float angle)
+{
+    if (m_pitch > m_pitchMin && m_pitch < m_pitchMax) {
+        // Compute new angle
+        float newpitch = m_pitch + angle;
+        if (newpitch < m_pitchMin) {
+            newpitch = m_pitchMin;
+        }
+        if (newpitch > m_pitchMax) {
+            newpitch = m_pitchMax;
+        }
+
+        // Rotate
+        float rotationAngle = newpitch - m_pitch;
+        this->rotateAround(rotationAngle, {0, 0, 0}, this->getUnitx());
+
+        // Update pitch
+        m_pitch = newpitch;
+    }
+}
+
+void Camera::ZoomIn()
+{
+    this->scale(ZOOM_IN_FACTOR);
+}
+
+void Camera::ZoomOut()
+{
+    this->scale(ZOOM_OUT_FACTOR);
+}
+
+SceneObjData Camera::compileData() const
+{
+    return SceneObjData();
+}
+
+void Camera::transformationCallback()
+{
+    SceneObj::transformationCallback();
+
+    // Update units
+    if (!m_callbackOccured) {
+        m_callbackOccured = true;
+        this->lookAt();
+        m_callbackOccured = false;
+    }
+}
+
+void Camera::lookAt(const vec3 &point)
+{
+    vec3 direction = this->getOrigin() - point;
+    this->coincideWithZ(direction);
+}
+
+void Camera::computeTop(float fov)
+{
+    float fovRadians = glm::radians(fov);
+    m_top = glm::tan(fovRadians / 2.0) * m_near;
 }
 
 void Camera::computeRight(float aspectRatio)
