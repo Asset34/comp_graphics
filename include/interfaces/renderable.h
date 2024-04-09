45664727cd736ffec32f3513d9274fcec11f661d
Artemiy Kashintsev <artemiy.kashintsev@gmail.com>
2024-04-09 23:44:19 +0300

    Define Interface data structures

--- include/interfaces/renderable.h
+++ include/interfaces/renderable.h
@@ -1,9 +1,36 @@
 #ifndef RENDERABLE_H_
 #define RENDERABLE_H_
 
+#include <vector>
 #include "glm/glm.hpp"
 
-struct RenderData {};
+struct RenderData {
+    // Types
+    typedef glm::vec3 Vertex;
+    typedef glm::vec3 Color;
+    struct Edge {
+        Vertex begin;
+        Vertex end;
+    };
+    struct Polygon {
+        std::vector<float> Indices;
+        Color Color;
+    };
+
+    // Flags
+    bool DrawVertices = false;
+    bool DrawEdges    = false;
+    bool DrawPolygons = false;
+    bool UseModelMatr = false;
+    bool UseViewMatr  = false;
+    bool UseProjMatr  = false;
+
+    // Data
+    std::vector<Vertex>  Vertices;
+    std::vector<Edge>    Edges;
+    std::vector<Polygon> Polygons;
+
+};
 
 class Renderable {
 public:
--- include/interfaces/renderprovider.h
+++ include/interfaces/renderprovider.h
@@ -2,9 +2,24 @@
 #define RENDERPROVIDER_H_
 
 #include <vector>
+#include "glm/glm.hpp"
 #include "renderable.h"
 
-struct RenderProviderData {};
+struct RenderProviderData {
+    // Types
+    typedef glm::vec3 Color;
+
+    // Data
+    std::vector<Renderable> Renderables;
+
+    // Matrices
+    glm::mat4 ViewMatrix = glm::mat4(1); // Default: Identity Matrix
+    glm::mat4 ProjMatrix = glm::mat4(1); // Default: Identity Matrix
+
+    // Visual
+    Color BackgroundColor;
+
+};
 
 class RenderProvider {
 public:
