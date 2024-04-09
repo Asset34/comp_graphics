7c792f818ff18c4d2c9ccef12fe0133eabc88e9d
Artemiy Kashintsev <artemiy.kashintsev@gmail.com>
2024-04-10 02:59:09 +0300

    Update shaders paths

--- include/shaders.h
+++ include/shaders.h
@@ -9,6 +9,6 @@ namespace Shaders {
             * model, view, proj matrices
             * uniform color
     */
-    const string BASIC_VERTEX_SHADER   = "shaders/basic_vertex.glsl";
-    const string BASIC_FRAGMENT_SHADER = "shaders/basic_fragment.glsl";
+    const string BASIC_VERTEX_SHADER   = "../shaders/basic_vertex.glsl";
+    const string BASIC_FRAGMENT_SHADER = "../shaders/basic_fragment.glsl";
 }
