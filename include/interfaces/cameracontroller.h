98ef93daa7615e505b00e7232ba37d1e4932efaa
Artemiy Kashintsev <artemiy.kashintsev@gmail.com>
2024-04-10 23:09:05 +0300

    Camera Control: extended interface

--- include/interfaces/cameracontroller.h
+++ include/interfaces/cameracontroller.h
@@ -8,6 +8,10 @@ public:
     // Projection control
     virtual void setAspectRatio(float aspectRatio) = 0;
     virtual void setFov(float fov) = 0;
+    virtual void setVolume(float hfov, float aspectRatio, float near, float far) = 0;
+    virtual void setNoProjection() = 0;
+    virtual void setOrthoProjection() = 0;
+    virtual void setPerspectiveProjection() = 0;
 
     // Rotation controls
     virtual void rotateYaw(float angle) = 0;
