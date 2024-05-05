d0a8e66fd4f9c18881dd7b541e465aabd4c8dc77
Artemiy Kashintsev <artemiy.kashintsev@gmail.com>
2024-05-05 13:03:35 +0300

    Define ObservationController2D interface

---
+++ include/interfaces/observationcontroller2d.h
@@ -0,0 +1,17 @@
+#ifndef OBSERVATIONCONTROLLER2D_H_
+#define OBSERVATIONCONTROLLER2D_H_
+
+class ObservationController2D {
+public:
+    virtual ~ObservationController2D() {};
+
+    virtual void reset() = 0;
+    
+    virtual void movex(float dx) = 0;
+    virtual void movey(float dy) = 0;
+    virtual void zoomIn() = 0;
+    virtual void zoomOut() = 0;
+
+}
+
+#endif // OBSERVATIONCONTROLLER2D_H_
