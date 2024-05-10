e8ec52f222ef1faa99367aa3392bfada5fecabb4
Artemiy Kashintsev <artemiy.kashintsev@gmail.com>
2024-05-10 13:57:44 +0300

    Scene2D: set size of AxisXY as big number to fake dynamical behaviour

--- src/scenes/scene2d.cpp
+++ src/scenes/scene2d.cpp
@@ -3,7 +3,7 @@
 Scene2D::Scene2D()
 {
     // Setup default XY
-    m_xy.setSize(15);
+    m_xy.setSize(100000);
     m_xy.setColorX({0.95, 0.3, 0.4});
     m_xy.setColorY({0.5, 0.8, 0.25});
     m_xy.setWidth(3);
